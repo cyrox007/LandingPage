1cfe808a8f00d7d17e4b474e7ca5ab4102dc4c4b
cyrox007 <cyrox007@gmail.com>
2021-04-14 12:07:12 +0300

    New 2021/04/14

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let pageSlider = new Swiper('.page', {
+    wraperClass: "page__wraper",
+    slideClass: "page__screen",
+
+    direction: 'vertical',
+
+    slidesPerView: 'auto',
+
+    parallax: true,
+});
